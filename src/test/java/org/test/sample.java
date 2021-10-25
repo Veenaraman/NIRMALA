72630906d4abc018f05ff6f4b8153659875ee753
Veenaraman <veenaraman1998@gmail.com>
2021-10-25 22:39:56 +0530

    project structure

---
+++ src/test/java/org/test/sample.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("priya");
+	System.out.println("veena");
+	System.out.println("test1");
+	System.out.println("test2");
+
+
+	
+
+}
+}
