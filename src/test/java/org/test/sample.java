ba6fc05b2107e6af24608e209b80a3b65dd608e3
Veenaraman <veenaraman1998@gmail.com>
2021-10-25 23:04:03 +0530

    jaanu-ma's is a dash

--- src/test/java/org/test/sample.java
+++ src/test/java/org/test/sample.java
@@ -2,13 +2,9 @@ package org.test;
 
 public class sample {
 public static void main(String[] args) {
-	System.out.println("priya");
-	System.out.println("veena");
+//	System.out.println("priya");
+//	System.out.println("veena");
 	System.out.println("test1");
 	System.out.println("test2");
-
-
-	
-
 }
 }
