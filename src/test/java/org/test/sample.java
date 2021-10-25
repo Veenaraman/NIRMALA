586a3abc70f56915e625273ede6b2694ed337a56
Veenaraman <veenaraman1998@gmail.com>
2021-10-25 22:54:36 +0530

    priyaaa is very beautiful

--- src/test/java/org/test/sample.java
+++ src/test/java/org/test/sample.java
@@ -2,13 +2,10 @@ package org.test;
 
 public class sample {
 public static void main(String[] args) {
-	System.out.println("priya");
-	System.out.println("veena");
-	System.out.println("test1");
-	System.out.println("test2");
-
-
-	
+	System.out.println("teena");
+	System.out.println("meena");
+	System.out.println("deena");
+	System.out.println("mona");
 
 }
 }
